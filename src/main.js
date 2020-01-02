266635221bad56186fb39628ed9fb2106d7e2271
d9beuD <53569029+d9beuD@users.noreply.github.com>
2020-01-02 17:16:35 +0100

    Add Font Awesome

--- src/main.js
+++ src/main.js
@@ -1,18 +1,29 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import BootstrapVue from 'bootstrap-vue'
+
 import App from './App.vue'
 import Home from './components/Home.vue'
+import Release from './components/releases/Release.vue'
+
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './assets/css/bootstrap/dist/css/custom.min.css'
 
-Vue.config.productionTip = false
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faDownload } from '@fortawesome/free-solid-svg-icons'
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
 
+library.add(faDownload, faChevronLeft)
+
+Vue.config.productionTip = false
 Vue.use(VueRouter)
 Vue.use(BootstrapVue)
+Vue.component('fa-icon', FontAwesomeIcon)
 
 const routes = [
-  { path: '/', component: Home }
+  { path: '/', component: Home },
+  { path: '/release/:id', component: Release}
 ]
 
 const router = new VueRouter({
