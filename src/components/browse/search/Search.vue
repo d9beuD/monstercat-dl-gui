86186f76f3400b95abf5de0a33397ba685e4780c
Vincent Bathelier <v.bathelier@abplussoftware.fr>
2020-01-10 14:38:52 +0100

    Add search bar

---
+++ src/components/browse/search/Search.vue
@@ -0,0 +1,15 @@
+<template>
+  <b-container>
+    <search-bar></search-bar>
+  </b-container>
+</template>
+
+<script>
+import SearchBar from './SearchBar.vue'
+
+export default {
+  components: {
+    SearchBar
+  }
+}
+</script>
---
+++ src/components/browse/search/SearchBar.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <b-form-group>
+      <b-input placeholder="test"></b-input>
+    </b-form-group>
+  </div>
+</template>
